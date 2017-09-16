550cf1fdb7c2b5a0eb1bc637fde17b04230ec642
Ferdinand Holzer <ferdinand.holzer@gmail.com>
2017-09-16 15:04:59 +0200

    mouse line and focus rings work

--- src/charts/Chart.js
+++ src/charts/Chart.js
@@ -19,6 +19,21 @@ class TimeSeries {
             throw new TypeError("Found invalid value");
         }
     }
+
+    find(datum) {
+        var ax = this.accessors.x;
+		var bisect = d3.bisector(ax).left;
+		var i = bisect(this.data, datum) - 1
+		if(i === -1)
+			return null
+
+		//look to far after serie is defined
+		if(i === this.data.length - 1 &&
+            this.data.length > 1 &&
+            Number(datum) - Number(ax(this.data[i])) > Number(ax(this.data[i])) - Number(ax(this.data[i - 1])))
+            return null
+		return this.data[i]
+	}
 }
 
 export default class Chart extends Component {
@@ -62,17 +77,13 @@ export default class Chart extends Component {
                     .tickFormat("")
             );
 
-        svg.select(".x.axis")
+        svg.select(".x.axis.major")
             .attr("transform", "translate(0," + height + ")")
             .call(xAxis)
             .selectAll(".tick text")
             .call(this.wrap, 35);
 
-        svg.select(".xMinorAxis")
-            //.style({ 'stroke': 'Black', 'fill': 'none', 'stroke-width': '1px'})
-            .style('stroke', 'Black')
-            .style('fill', 'none')
-            .style('stroke-width', '1px')
+        svg.select(".x.axis.minor")
             .attr("transform", "translate(0," + height + ")")
             .call(xMinorAxis)
             .selectAll("text").remove();
@@ -130,7 +141,7 @@ export default class Chart extends Component {
             .attr("y2", height);
 
         //circles are selected again to add the mouseover functions
-        g.selectAll("circle")
+        /*g.selectAll("circle")
         .on("mouseover", function(d) {
             this.div.transition()
                 .duration(200)
@@ -161,7 +172,7 @@ export default class Chart extends Component {
                 .duration(500)
                 .style("opacity", 0);
         });
-
+        */
     }
 
     componentDidMount() {
@@ -178,11 +189,13 @@ export default class Chart extends Component {
 
         // create axes
         this.axisX = d3.axisBottom(x)
-            .tickArguments([d3.timeHour.every(24)])
+            //.tickArguments([d3.timeHour.every(24)])
+            .ticks(8)
             .tickSize(10);
 
         this.axisXMinor = d3.axisBottom(x)
-            .tickArguments([d3.timeHour.every(12)]);
+            //.tickArguments([d3.timeHour.every(12)]);
+            .ticks(16)
 
         this.axisY = d3.axisLeft(y);
 
@@ -193,26 +206,44 @@ export default class Chart extends Component {
         this.formatTime = d3.timeFormat("%Y-%m-%dT%H:%M:%S%Z");
         this.formatCount = d3.format(",");
 
+        this.planes = {};
 
         // create required DOM elements
+        var rangeTranslate = "translate(" + margin.left + "," + margin.top + ")";
         this.div = d3.select(this.divNode)
             .attr("class", "tooltip")
             .style("opacity", 0);
 
-        var svg = d3.select(this.svgNode)
+        var svg = this.planes.root = d3.select(this.svgNode)
             .attr("width", width + margin.left + margin.right)
             .attr("height", height + margin.top + margin.bottom);
 
-        var range = svg.append("g")
-            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+        var range = this.planes.range = svg.append("g")
+            .attr("transform", rangeTranslate);
 
-        var mouseCatcher = svg.append("g")
-            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+        this.planes.annotations = svg.append("g")
+            .attr("transform", rangeTranslate);
+
+        this.planes.focusRings = this.planes.annotations.append("g");
+
+        var mouseCatcher = this.planes.mouseCatcher = svg.append("g")
+            .attr("transform", rangeTranslate);
 
         svg = this.svg = range;
 
         // create grid and axes container
-        ["grid", "x axis", "xMinorAxis", "y axis"].forEach((c) => svg.append("g").attr("class", c));
+        var axes = svg.append("g")
+            .attr("class", "axes");
+
+        ["grid", "x axis major", "x axis minor", "y axis major"].forEach((c) => axes.append("g").attr("class", c));
+
+        this.mouseLine = axes.append("g")
+            .attr("class", "mouseline")
+            .append("line")
+            .attr("x1", 0)
+            .attr("x2", 0)
+            .attr("y1", height)
+            .attr("y2", 0)
 
         //http://www.d3noob.org/2012/12/adding-axis-labels-to-d3js-graph.html
         svg.append("text")      // text label for the x-axis
@@ -240,11 +271,11 @@ export default class Chart extends Component {
 
         this.mouseCatcher = mouseCatcher.append('rect')
             .attr('width',width)
-            .attr('class','d3_timeseries mouse-catch')
+            .attr('class','mouse-catch')
             .attr('height',height)
             .style('opacity',0)
             .on('mousemove', (unk1, unk2, els) => this.graphMouseMove(els))
-            .on('mouseout', this.graphMouseOut);
+            .on('mouseout', (unk1, unk2, els) => this.graphMouseOut(els));
 
 
         var onDataLoadComplete = (name, res) => {
@@ -297,16 +328,17 @@ export default class Chart extends Component {
     }
 
     graphMouseMove(elements) {
-        console.log("this", this);
+        //console.log("this", this);
         //console.log("arguments", arguments);
-        var x = d3.mouse(elements[0])[0];
-        x = this.scaleX.invert(x);
-        console.log("x", x);
-        console.log(this.rings.selectAll(".dots"))
+        var xpos = d3.mouse(elements[0])[0];
+        var xdate = this.scaleX.invert(xpos);
+        console.log("x", xdate);
+        //console.log(this.rings.selectAll(".dots"))
+        this.updateMouseLine(xpos);
+        //this.updateTip(xdate);
+		this.updatefocusRing(xdate);
 
         /*
-        mousevline.datum({x:x,visible:true});
-        mousevline.update();
 
         this.div.transition()
             .duration(200)
@@ -325,6 +357,55 @@ export default class Chart extends Component {
             */
     }
 
+    updatefocusRing(xdate) {
+        var s = this.planes.focusRings.selectAll(".focusring");
+
+        if(xdate === null) {
+            s = s.data([]);
+        } else {
+            s = s.data(this.s.map(function(s,i) {
+                return {
+                    x: xdate,
+                    item: s.find(xdate),
+                    accessors: s.accessors
+                };
+            }).filter(function(d) {
+                return d.item !== undefined && d.item !== null &&
+                    d.accessors.y(d.item) !== null && !isNaN(d.accessors.y(d.item));
+            }));
+        }
+
+        s.enter().append("circle")
+            .attr('class','focusring')
+            .attr('r',5)
+            .attr('cx',(d) => this.scaleX(d.accessors.x(d.item)))
+            .attr('cy',(d) => this.scaleY(d.accessors.y(d.item)));
+
+        s.transition().duration(50)
+            .attr('cx',(d) => this.scaleX(d.accessors.x(d.item)))
+            .attr('cy',(d) => this.scaleY(d.accessors.y(d.item)));
+
+        s.exit().remove();
+
+    }
+
+    updateTip(x) {
+        if(x === null) {
+            this.div.transition()
+                .duration(500)
+                .style("opacity", 0);
+            return;
+        }
+
+        var d = [];
+        for(var s of this.s) {
+            var nearest = s.find(x);
+            d.push({
+                name: s.options && s.options.displayname || s.name,
+            });
+        }
+
+    }
 
     /*
     circleMouseOut(d) {
@@ -340,24 +421,31 @@ export default class Chart extends Component {
             .duration(500)
             .style("opacity", 0);
     }
+*/
 
-    graphMouseMove(e) {
-		var x = d3.mouse(container[0][0])[0];
-		x = xscale.invert(x);
-		mousevline.datum({x:x,visible:true});
-		mousevline.update();
-		updatefocusRing(x);
-		updateTip(x);
-	}
+    updateMouseLine(x) {
+        if(x === null) {
+            this.mouseLine.style("opacity", 0);
+            return;
+        }
+        this.mouseLine
+            .attr("x1", x)
+            .attr("x2", x)
+            .style("opacity", 1);
+    }
 
 	graphMouseOut(e) {
-		mousevline.datum({ x: null, visible: false });
-		mousevline.update();
-		updatefocusRing(null);
+		this.updateMouseLine(null);
+        this.updatefocusRing(null);
+		/*
 		updateTip(null);
+        */
 	}
-    */
 
+    componentWillUnmount() {
+        d3.select(this.svgNode).selectAll().remove();
+        d3.select(this.divNode).selectAll().remove();
+    }
 
     render() {
         return (
